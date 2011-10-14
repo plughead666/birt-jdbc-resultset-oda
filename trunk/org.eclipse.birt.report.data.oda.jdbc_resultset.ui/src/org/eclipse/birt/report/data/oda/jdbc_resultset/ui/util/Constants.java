2c3e76ac3325b1b0962d792f45b4540a475804d0
digiassn@gmail.com <digiassn@gmail.com@0f4de3c1-375b-ac50-13b3-fc38152b2f32>
2011-10-14 16:27:45 +0000

    Commiting the UI

---
+++ trunk/org.eclipse.birt.report.data.oda.jdbc_resultset.ui/src/org/eclipse/birt/report/data/oda/jdbc_resultset/ui/util/Constants.java
@@ -0,0 +1,16 @@
+package org.eclipse.birt.report.data.oda.jdbc_resultset.ui.util;
+
+public class Constants
+{
+    public static final String ODAURL = 
+        org.eclipse.birt.report.data.oda.jdbc_resultset.Connection.Constants.ODAURL;
+    public static final String ODAPassword = 
+        org.eclipse.birt.report.data.oda.jdbc_resultset.Connection.Constants.ODAPassword;
+    public static final String ODAUser = 
+        org.eclipse.birt.report.data.oda.jdbc_resultset.Connection.Constants.ODAUser;
+    public static final String ODADriverClass = 
+        org.eclipse.birt.report.data.oda.jdbc_resultset.Connection.Constants.ODADriverClass; 
+    public static final String ODAJndiName = 
+        org.eclipse.birt.report.data.oda.jdbc_resultset.Connection.Constants.ODAJndiName;
+
+}
